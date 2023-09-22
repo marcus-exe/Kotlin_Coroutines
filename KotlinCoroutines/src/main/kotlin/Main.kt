73e4329c53690178c2e8ef3b4cd8f9bbfe8bb895
Marcus Sena <mens.eai20@uea.edu.br>
2023-09-22 10:18:33 -0400

    More coroutines concepts

--- KotlinCoroutines/src/main/kotlin/Main.kt
+++ KotlinCoroutines/src/main/kotlin/Main.kt
@@ -1,36 +1,37 @@
 import kotlinx.coroutines.*
-import kotlin.system.measureTimeMillis
 
-
-suspend fun getForecast() : String{
-    delay(1000)
-    return "Sunny"
-}
-
-//basically JS async
-suspend fun getTemperature() : String{
-    delay(1000)
-    return "30\u00b0C"
+fun main() {
+    runBlocking {
+        println("Weather forecast")
+        println(getWeatherReport())
+        println("Have a good day!")
+    }
 }
 
 suspend fun getWeatherReport() = coroutineScope {
     val forecast = async { getForecast() }
-    val temperature = async { getTemperature() }
-
-    "${forecast.await()} ${temperature.await()}"
-}
-
-
-
-fun main() {
-
-    val time = measureTimeMillis {
-        runBlocking {
-            println("Weather forecast")
-            getWeatherReport()
-            println("Have a good Day")
+    val temperature = async {
+        try {
+            getTemperature()
+        } catch (e: AssertionError) {
+            println("Caught exception $e")
+            "{ No temperature found }"
         }
     }
-    println("Execution time: ${time / 1000.0} seconds")
 
+    delay(200)
+    temperature.cancel()
+
+    "${forecast.await()}"
+}
+
+suspend fun getForecast(): String {
+    delay(1000)
+    return "Sunny"
+}
+
+suspend fun getTemperature(): String {
+    delay(500)
+    throw AssertionError("Temperature is invalid")
+    return "30\u00b0C"
 }
